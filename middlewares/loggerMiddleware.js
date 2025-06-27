629273037e708a353d3f8c1c0ba001216a7b8798
Atul Kumar <atulkumar.works@gmail.com>
2025-06-27 10:07:27 +0530

    feat: implement logger middleware to log requests and responses

--- middlewares/loggerMiddleware.js
+++ middlewares/loggerMiddleware.js
@@ -1,8 +1,19 @@
+const fs = require('fs');
+
 
 const loggerMiddleware = (req, res, next)=>{
 const date = new Date();
 
-    console.log(`Date: ${date}, Method: ${req.method}, URL: ${req.url}}`);
+    const logMessage = `Date: ${date}, Method: ${req.method}, IP Address: ${req.ip}, URL: ${req.url}\n`;
+    fs.appendFile('./public/logs.txt', logMessage, err =>{
+        if (err) {
+            console.error('Error writing to log file:', err);
+        } else {
+            console.log('Log message written successfully');
+        }
+    } )
+
+
     next();
 
 }
